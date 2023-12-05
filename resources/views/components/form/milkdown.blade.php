805c74e61d3f19b4bc7cbc3c8cafcee0857c2afb
LiAn DhAs <lianmay.mesi@gmail.com>
2023-12-05 12:10:36 +0530

    v0.3.6
    Fixed some bugs in milkdown

--- resources/views/components/form/milkdown.blade.php
+++ resources/views/components/form/milkdown.blade.php
@@ -16,7 +16,22 @@
             </label>
         </div>
     @endif
-    <div x-data="{ ...toastUiEditor(@entangle($attributes->wire('model'))) }"
+    <div x-data="{
+        markdown: @entangle({{ $attributes->wire('model') }}),
+        init() {
+            let editor = new Editor({
+                el: this.$refs.editor,
+                height: '500px',
+                initialEditType: 'markdown',
+                initialValue: this.markdown,
+                previewHighlight: true,
+                viewer: false,
+                usageStatistics: false,
+                previewStyle: 'vertical',
+            });
+            editor.getMarkdown();
+        }
+    }"
         class="!grid !w-full !overflow-hidden rounded-lg border-none bg-slate-50 ring-1 ring-slate-950/10 focus-within:ring-2 focus-within:ring-indigo-600">
         <div x-ref="editor" class="!prose !w-full !min-w-full"></div>
     </div>
