f98b154e6919cc2535d3c1f6796169823068bd8f
LiAn DhAs <lianmay.mesi@gmail.com>
2023-12-26 15:09:04 +0530

    Update input.blade.php

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -20,7 +20,7 @@
     <div
         class="flex overflow-hidden rounded-lg bg-slate-50 ring-1 ring-slate-950/10 focus-within:ring-2 focus-within:ring-indigo-600">
         <div class="min-w-0 flex-1">
-            <input {{ $attributes }} id="{{ str_slug($label) }}" :readonly="!$readonly"
+            <input {{ $attributes }} id="{{ str_slug($label) }}" @if (!$readOnly) readonly @endif
                 class="block w-full border-none bg-transparent py-1.5 pe-3 ps-3 text-sm leading-6 text-slate-950 outline-none transition duration-75 placeholder:text-slate-500 focus:ring-0 disabled:text-slate-500 disabled:placeholder:text-slate-400" />
         </div>
     </div>
