72020ec72f1f28977e9aaa37abe3fd2a0cb16e3d
LiAn DhAs <lianmay.mesi@gmail.com>
2023-12-28 21:21:47 +0530

    wire:navigate is Optional
    `wire:navigate` is optional by using `no-navigate` for anchor components

--- resources/views/components/anchor/warning.blade.php
+++ resources/views/components/anchor/warning.blade.php
@@ -1,5 +1,5 @@
-@props(['href', 'icon'])
-<a wire:navigate href="{{ $href }}"
+@props(['href', 'icon', 'noNavigate' => false])
+<a @if (!$noNavigate) wire:navigate @endif href="{{ $href }}"
     class="font-medium leading-none text-orange-600 duration-150 hover:text-orange-700 hover:underline">
     <div class="flex items-center space-x-1">
         @if (isset($icon))
