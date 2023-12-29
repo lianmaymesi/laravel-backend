0b41362f0ec7d35ef0500f6d961e03e539a35d4b
LiAn DhAs <lianmay.mesi@gmail.com>
2023-12-29 09:57:54 +0530

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -83,22 +83,7 @@
                                     'max-w-[240px] bg-white p-2': !isAboveBreakPoint
                                 }">
                                 <ul class="grid gap-y-2 py-4">
-                                    <x-admin.navigate :route="route('admin.index')" :is_active="request()->is('/')">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="mr-3 h-4 w-4">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                                        </svg>
-                                        <span>Dashboard</span>
-                                    </x-admin.navigate>
-                                    <x-admin.navigate :route="route('admin.banners.create')" :is_active="request()->is('banners*')">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="mr-3 h-4 w-4">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                                        </svg>
-                                        <span>Banners</span>
-                                    </x-admin.navigate>
+                                    {{--  --}}
                                 </ul>
                             </div>
                             <div class="fixed inset-y-1/2 ml-[256px]" :class="{ 'hidden': isAboveBreakPoint }">
