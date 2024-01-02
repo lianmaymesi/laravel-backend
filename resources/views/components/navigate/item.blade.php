911fc6142e0ac5088f6a066ceeccf48985664449
LiAn DhAs <lianmay.mesi@gmail.com>
2024-01-02 14:05:19 +0530

    wip

--- resources/views/components/navigate/item.blade.php
+++ resources/views/components/navigate/item.blade.php
@@ -1,10 +1,8 @@
-@props(['route', 'is_active' => false, 'noNavigate' => false])
+@props(['route', 'is_active' => false, 'noNavigate' => false, 'path'])
 <li>
-    <a href="{{ $route }}" @if (!$noNavigate) wire:navigate @endif @class([
-        'flex items-center rounded-lg p-2 text-sm font-medium leading-none hover:bg-indigo-700 hover:text-indigo-50',
-        'bg-indigo-700 text-indigo-50' => $is_active,
-        'text-slate-700' => !$is_active,
-    ])>
+    <a href="{{ $route }}" @if (!$noNavigate) wire:navigate @endif
+        class="flex items-center rounded-lg p-2 text-sm font-medium leading-none hover:bg-indigo-700 hover:text-indigo-50"
+        x-bind:class="$current('{{ $path }}') ? 'bg-indigo-700 text-indigo-50' : 'text-slate-700'">
         {{ $slot }}
     </a>
 </li>
