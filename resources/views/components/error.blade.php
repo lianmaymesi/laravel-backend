f5abfdda3e7975459b06b3935aa71b2c3b41fc03
LiAn DhAs <lianmay.mesi@gmail.com>
2024-03-20 15:16:07 +0530

    Update error.blade.php

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,3 +1,3 @@
-<div class="text-sm text-red-500" x-init="$el.closest('form').closest('label').scrollIntoView()">
+<div class="text-sm text-red-500" aria-live="assertive" x-init="$el.closest('form').querySelector('[aria-invalid=\'true\']').closest('label').scrollIntoView()">
     {{ $slot }}
 </div>
